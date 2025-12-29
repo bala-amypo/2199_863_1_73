1caf612d7b3f818f52c9dde430ce8c93a20c48af
bala-amypo <bala@amypo.in>
2025-12-29 05:08:34 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,11 +1,34 @@
+// package com.example.demo;
+
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.boot.web.servlet.ServletRegistrationBean;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.transaction.annotation.EnableTransactionManagement;
+// import com.example.demo.servlet.SimpleServlet;
+
+// @SpringBootApplication
+// @EnableTransactionManagement
+// public class DemoApplication {
+
+//     public static void main(String[] args) {
+//         SpringApplication.run(DemoApplication.class, args);
+//     }
+
+//     @Bean
+//     public ServletRegistrationBean<SimpleServlet> simpleServletRegistration() {
+//         ServletRegistrationBean<SimpleServlet> registration = new ServletRegistrationBean<>();
+//         registration.setServlet(new SimpleServlet());
+//         registration.addUrlMappings("/simple-servlet");
+//         return registration;
+//     }
+// }
+
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import com.example.demo.servlet.SimpleServlet;
 
 @SpringBootApplication
 @EnableTransactionManagement
@@ -13,13 +36,5 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
-    }
-
-    @Bean
-    public ServletRegistrationBean<SimpleServlet> simpleServletRegistration() {
-        ServletRegistrationBean<SimpleServlet> registration = new ServletRegistrationBean<>();
-        registration.setServlet(new SimpleServlet());
-        registration.addUrlMappings("/simple-servlet");
-        return registration;
     }
 }
